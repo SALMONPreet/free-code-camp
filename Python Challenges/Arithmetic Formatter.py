e3a1639a312d2a15ee037d7eb984466a284a0c58
Samanpreet Singh Lang <spsinghlang@gmail.com>
2025-08-10 18:10:01 +0530

    Create Arithmetic Formatter.py

---
+++ Python Challenges/Arithmetic Formatter.py
@@ -0,0 +1,47 @@
+def arithmetic_arranger(problems, show_answers=False):
+    if len(problems) > 5:
+        return 'Error: Too many problems.'
+
+    first_line = []
+    second_line = []
+    dashes = []
+    answers = []
+
+    for problem in problems:
+        parts = problem.split()
+
+        if len(parts) != 3:
+            return "Error: Invalid format."
+
+        num1, operator, num2 = parts
+
+        if operator not in ('+', '-'):
+            return "Error: Operator must be '+' or '-'."
+
+        if not num1.isdigit() or not num2.isdigit():
+            return 'Error: Numbers must only contain digits.'
+
+        if len(num1) > 4 or len(num2) > 4:
+            return 'Error: Numbers cannot be more than four digits.'
+
+        width = max(len(num1), len(num2)) + 2
+
+        first_line.append(num1.rjust(width))
+        second_line.append(operator + num2.rjust(width - 1))
+        dashes.append('-' * width)
+
+        if show_answers:
+            result = str(eval(num1 + operator + num2))
+            answers.append(result.rjust(width))
+
+    arranged_problems = '    '.join(first_line) + '\n' + \
+                        '    '.join(second_line) + '\n' + \
+                        '    '.join(dashes)
+
+    if show_answers:
+        arranged_problems += '\n' + '    '.join(answers)
+
+    return arranged_problems
+
+# Example test
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
